09a30da649e360a62fb6629a67ad8f3a90ec60f3
mklabar <38596988+mklabar@users.noreply.github.com>
2018-11-05 17:57:08 -0800

    Added fibonacci implementation as well as main function calls

--- recur.cc
+++ recur.cc
@@ -1,19 +1,40 @@
 #include <iostream>
 
-//TODO #1.1: add a function prototype for fib
-
+size_t fib(const size_t);
 size_t fact(const size_t);
 
 int main()
 {
-	//TODO: #1.2 call your fib function, print the result
+	size_t choice=0, value=0;
+  std::cout<<"1. Factorial \n2. Fibonacci"<<std::endl;
+  std::cout<<"> ";
+  std::cin>> choice;
+  std::cout<<"\n\n";
 
-	//TODO: #2.2 call your fact function, print the result
-
+	if(choice==1)
+  {
+      std::cout<<"Please enter factorial you would like to calculate"<<std::endl;
+      std::cout<<"> ";
+      std::cin>> value;
+      std::cout<<"\n"<<value<<"! = "<<fact(value)<<"\n";
+  }
+	else if(choice==2)
+  {
+      std::cout<<"Please enter Fibonacci number you would like to calculate"<<std::endl;
+      std::cout<<"> ";
+      std::cin>> value;
+      std::cout<<"\nThe"<<value<<"th number in the Fibonacci sequence is "<<fib(value)<<"\n";
+  }
 	return 0;
 }
 
-//TODO #2.3: add your recursive fib function from class
+size_t fib(const size_t N)
+{
+  if(N==0||N==1)
+    return N;
+  else
+    return fib(N-2)+fib(N-1);
+}
 
 size_t fact(const size_t N)
 {
